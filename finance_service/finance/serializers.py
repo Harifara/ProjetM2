7eec13c718e2acce2b19e197866715735988cf53
harifara <andriamazaoromaheninaharifara@gmail.com>
2025-12-08 22:25:07 +0300

    nouveau

--- finance_service/finance/serializers.py
+++ finance_service/finance/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import DemandeDecaissement, DemandeDecaissementItem, Depense
-from coordinator.models import ValidationCoordinateur
+# from cordo_service.cordo.models import ValidationCoordinateur
 
 # ------------------------
 # Serializer Item Décaissement
