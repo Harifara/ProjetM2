c1f1507824400ce2b8b91c50fb086ed0eb4510cf
harifara <andriamazaoromaheninaharifara@gmail.com>
2025-12-11 21:16:53 +0300

    nouveau

--- cordo_service/cordo_service/settings.py
+++ cordo_service/cordo_service/settings.py
@@ -11,6 +11,7 @@ DEBUG = config("DEBUG", default=True, cast=bool)
 ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default="localhost").split(",")
 
 INSTALLED_APPS = [
+    "finance",
     "cordo",
     "django.contrib.admin",
     "django.contrib.auth",
