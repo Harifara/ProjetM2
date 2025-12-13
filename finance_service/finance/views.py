138cbd48be454869741dec12064b3418cc2ea2f3
harifara <andriamazaoromaheninaharifara@gmail.com>
2025-12-13 16:17:02 +0300

    nouveau

--- finance_service/finance/views.py
+++ finance_service/finance/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.views import APIView
+  # si tu utilises des codes de statut
 
 from .models import DemandeDecaissement, Depense
 from .serializers import (
